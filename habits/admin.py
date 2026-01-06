d9d3a1405850f634dae332ce89d1a142099f85af
Olga Python <olga_python@proton.me>
2026-01-06 21:31:29 +0100

    Habit model

---
+++ habits/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Habit
+
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'action', 'time', 'is_pleasant', 'is_public',)
+    list_filter = ('is_pleasant', 'is_public')
+    search_fields = ('action', 'place')
